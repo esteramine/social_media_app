e77d6c131ed30398d499f9876113ceb16af46462
esteramine <cchouaa@connect.ust.hk>
2021-02-15 22:04:25 +0800

    display single post, delete function

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,16 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button, Card, Icon, Image, Label } from 'semantic-ui-react';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
 
-function PostCard({post: { id, body, createdAt, username, likeCount, commentCount, likes, comments }}){
-    function likePost(){
-        console.log('likePost');
-    }
+import { AuthContext } from '../context/auth';
+import LikeButton from './LikeButton';
+import DeleteButton from './DeleteButton';
 
-    function commentPost(){
-        console.log('commentPost');
-    }
+function PostCard({post: { id, body, createdAt, username, likeCount, commentCount, likes, comments }}){
+    const { user } = useContext(AuthContext);
     
     return(
         <Card fluid>
@@ -25,18 +23,21 @@ function PostCard({post: { id, body, createdAt, username, likeCount, commentCoun
                 <Card.Description>{body}</Card.Description>
             </Card.Content>
             <Card.Content extra>
-                <Button as='div' labelPosition='right' onClick={likePost}>
-                    <Button color='teal' basic>
-                        <Icon name='heart' />
-                    </Button>
-                    <Label basic color='teal' pointing='left'>{likeCount}</Label>
-                </Button>
-                <Button as='div' labelPosition='right' onClick={commentPost}>
+                <LikeButton user={user} post={{ id, likes, likeCount }}/>
+                <Button labelPosition='right' as={Link} to={`/posts/${id}`}>
                     <Button color='blue' basic>
                         <Icon name='comments' />
                     </Button>
                     <Label basic color='blue' pointing='left'>{commentCount}</Label>
                 </Button>
+                {user && user.username === username && (
+                    <DeleteButton postId={id}/>
+                )}
+                {comments.map((comment)=>(
+                    <div>
+                        <p>{comment.username+ ": " + comment.body}</p>
+                    </div>
+                ))}      
             </Card.Content>
         </Card>
     );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Transition } from 'semantic-ui-react';
 
+import { AuthContext } from '../context/auth';
 import PostCard from '../components/PostCard';
+import PostForm from '../components/PostForm';
+import { FETCH_POSTS_QUERY } from '../util/graphql';
+
 function Home() {
+    const { user } = useContext(AuthContext);
     const { loading, data: {getPosts: posts}={} } = useQuery(FETCH_POSTS_QUERY);
     //posts is an alias for getPosts
 
@@ -14,42 +19,27 @@ function Home() {
                 <h1>Recent Posts</h1>
             </Grid.Row>
             <Grid.Row>
+                {user && (
+                    <Grid.Column>
+                        <PostForm/>
+                    </Grid.Column>
+                )}
                 {loading?(
                     <h1>Loading posts...</h1>
                 ):(
-                    posts && posts.map(post =>(
-                        <Grid.Column key={post.id}>
-                            <PostCard post={post}/>
-                        </Grid.Column>
-                    ))
+                    <Transition.Group>
+                        {
+                            posts && posts.map(post =>(
+                                <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+                                    <PostCard post={post}/>
+                                </Grid.Column>
+                            ))
+                        }
+                    </Transition.Group>  
                 )}
             </Grid.Row>
         </Grid>
     )
 }
-
-const FETCH_POSTS_QUERY = gql`
-    {
-        getPosts{
-            id 
-            username 
-            body 
-            createdAt 
-            likeCount 
-            commentCount
-
-            comments{
-                id 
-                username 
-                body 
-                createdAt
-            }
-
-            likes{
-                username
-            }
-        }
-    }
-`
 
 export default Home;
